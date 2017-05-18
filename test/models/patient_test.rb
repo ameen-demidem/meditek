960ce9fde38e4e57d3a79fca4351120c1e0b13d5
Ameen Demidem <ameen.demidem@thinkresearch.com>
2017-05-18 15:06:34 -0400

    Add tests for Patients

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PatientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should not save a patient without a first name" do
+    patient = patients(:first_name_missing)
+    assert_not patient.save
+  end
+
+  test "Should not save a patient without a last name" do
+    patient = patients(:last_name_missing)
+    assert_not patient.save
+  end
+
+  test "Should not save a patient without an mrn" do
+    patient = patients(:mrn_missing)
+    assert_not patient.save
+  end
 end

0b4763138b45e025cc85c7c3039a9f9938837b26
Ameen Demidem <ameen.demidem@thinkresearch.com>
2017-05-24 15:53:20 -0400

    Make the New Patient form a controlled form
    When a new patient fails to be saved the form is re-rendered with empty fields

--- client/app/bundles/Patients/components/new.jsx
+++ client/app/bundles/Patients/components/new.jsx
@@ -3,9 +3,30 @@ import React from 'react';
 import ReactOnRails from 'react-on-rails';
 
 export default class PatientsNew extends React.Component {
+  constructor (props) {
+    super(props);
+
+    const patient = this.props.patient;
+
+    this.state = {
+      firstName: patient.first_name || '',
+      lastName: patient.last_name || '',
+      middleName: patient.middle_name || '',
+      weight: patient.weight || '',
+      height: patient.height || '',
+      mrn: patient.mrn || ''
+    }
+  }
+
   render () {
-    console.log('Rendering PatientsNew');
-    console.log('Errors, if any: ', this.props.errors);
+    var styles = {
+      firstName : `field ${this.props.errors.first_name ? "field_with_errors" : null}`,
+      middleName : `field ${this.props.errors.middle_name ? "field_with_errors" : null}`,
+      lastName : `field ${this.props.errors.last_name ? "field_with_errors" : null}`,
+      weight : `field ${this.props.errors.weight ? "field_with_errors" : null}`,
+      height : `field ${this.props.errors.height ? "field_with_errors" : null}`,
+      mrn : `field ${this.props.errors.mrn ? "field_with_errors" : null}`
+    }
 
     return (
       <form
@@ -21,29 +42,60 @@ export default class PatientsNew extends React.Component {
           name="authenticity_token"
           value={`${ReactOnRails.authenticityToken()}`}
         />
-        <div className={`field ${this.props.errors.first_name ? "field_with_errors" : null}`}>
+        <div className={styles.firstName}>
           <label htmlFor="patient_first_name">First name</label><br/>
-          <input type="text" name="patient[first_name]" id="patient_first_name"/>
+          <input
+            value={this.state.firstName}
+            onChange={this.inputOnChange}
+            type="text"
+            name="patient[first_name]"
+            id="patient_first_name"
+          />
         </div>
-        <div className={`field ${this.props.errors.middle_name ? "field_with_errors" : null}`}>
+        <div className={styles.middleName}>
           <label htmlFor="patient_middle_name">Middle name</label><br/>
-          <input type="text" name="patient[middle_name]" id="patient_middle_name"/>
+          <input
+            value={this.state.middleName}
+            onChange={this.inputOnChange}
+            type="text"
+            name="patient[middle_name]"
+            id="patient_middle_name"/>
         </div>
-        <div className={`field ${this.props.errors.last_name ? "field_with_errors" : null}`}>
+        <div className={styles.lastName}>
           <label htmlFor="patient_last_name">Last name</label><br/>
-          <input type="text" name="patient[last_name]" id="patient_last_name"/>
+          <input
+            value={this.state.lastName}
+            onChange={this.inputOnChange}
+            type="text"
+            name="patient[last_name]"
+            id="patient_last_name"/>
         </div>
-        <div className={`field ${this.props.errors.weight ? "field_with_errors" : null}`}>
+        <div className={styles.weight}>
           <label htmlFor="patient_weight">Weight</label><br/>
-          <input type="text" name="patient[weight]" id="patient_weight"/>
+          <input
+            value={this.state.weight}
+            onChange={this.inputOnChange}
+            type="text"
+            name="patient[weight]"
+            id="patient_weight"/>
         </div>
-        <div className={`field ${this.props.errors.height ? "field_with_errors" : null}`}>
+        <div className={styles.height}>
           <label htmlFor="patient_height">Height</label><br/>
-          <input type="number" name="patient[height]" id="patient_height"/>
+          <input
+            value={this.state.height}
+            onChange={this.inputOnChange}
+            type="number"
+            name="patient[height]"
+            id="patient_height"/>
         </div>
-        <div className={`field ${this.props.errors.mrn ? "field_with_errors" : null}`}>
+        <div className={styles.mrn}>
           <label htmlFor="patient_mrn">Mrn</label><br/>
-          <input type="text" name="patient[mrn]" id="patient_mrn"/>
+          <input
+            value={this.state.mrn}
+            onChange={this.inputOnChange}
+            type="text"
+            name="patient[mrn]"
+            id="patient_mrn"/>
         </div>
         <div className="actions">
           <input type="submit" name="commit" value="Create Patient"/>
@@ -55,5 +107,19 @@ export default class PatientsNew extends React.Component {
 
   cancel = () => {
     window.history.back();
+  }
+
+  inputOnChange = (e) => {
+    const lut = {
+      "patient[first_name]": "firstName",
+      "patient[middle_name]": "middleName",
+      "patient[last_name]": "lastName",
+      "patient[weight]": "weight",
+      "patient[height]": "height",
+      "patient[mrn]": "mrn",
+    }
+    var state = this.state;
+    state[lut[e.target.name]] = e.target.value;
+    this.setState(state)
   }
 }
